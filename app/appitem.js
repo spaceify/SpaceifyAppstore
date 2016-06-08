78504aaead8b838e683967f4eb4c9c937eb65510
citsym <jouni.a.ojala@aalto.fi>
2016-06-08 14:20:29 +0300

    more fixes

--- app/appitem.js
+++ app/appitem.js
@@ -1,20 +1,11 @@
-System.register([], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
-    var AppItem;
-    return {
-        setters:[],
-        execute: function() {
-            AppItem = (function () {
-                function AppItem(name, readme, icon) {
-                    this.name = name;
-                    this.readme = readme;
-                    this.icon = icon;
-                }
-                return AppItem;
-            }());
-            exports_1("AppItem", AppItem);
-        }
+"use strict";
+var AppItem = (function () {
+    function AppItem(name, readme, icon) {
+        this.name = name;
+        this.readme = readme;
+        this.icon = icon;
     }
-});
+    return AppItem;
+}());
+exports.AppItem = AppItem;
 //# sourceMappingURL=appitem.js.map
