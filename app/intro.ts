d8c872d0abfe392d06501ddd76b69196f4c0c7d7
Max Vilkki <codise@codise.org>
2016-08-12 15:48:21 +0300

    Styling the main layout better.

--- app/intro.ts
+++ app/intro.ts
@@ -2,7 +2,11 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'intro',
-    template: '<h1>Hello</h1>',
+    styles: [`
+    h1 {
+    }
+    `],
+    template: '<h1>Frontpage</h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at libero diam. Nunc eget purus risus. Suspendisse cursus lacus non augue auctor placerat. Sed eleifend bibendum sapien quis porta. Mauris vitae felis eros. Nam volutpat augue quis augue aliquet, ac blandit mi laoreet. Vivamus faucibus ultricies nulla, eget faucibus lacus venenatis non. Nulla facilisi.</p>',
 })
 
 export class IntroComponent{ 
