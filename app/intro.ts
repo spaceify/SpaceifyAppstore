04029ea1783e3824b781bc3463e33ead861d9a56
Max Vilkki <codise@codise.org>
2016-09-14 15:34:53 +0300

    Separated intro component into several files.

--- app/intro.ts
+++ app/intro.ts
@@ -2,11 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'intro',
-    styles: [`
-    h1 {
-    }
-    `],
-    template: '<h1>Frontpage</h1><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla at libero diam. Nunc eget purus risus. Suspendisse cursus lacus non augue auctor placerat. Sed eleifend bibendum sapien quis porta. Mauris vitae felis eros. Nam volutpat augue quis augue aliquet, ac blandit mi laoreet. Vivamus faucibus ultricies nulla, eget faucibus lacus venenatis non. Nulla facilisi.</p>',
+    templateUrl: 'app/intro.html',
+    styleUrls: [ 'app/intro.css' ],
 })
 
 export class IntroComponent{ 
